a738d78ac5008cd853e333207a811f76902467fa
Dominik Schidlowski <dominik@schidlowski.eu>
2023-06-09 15:59:11 +0200

    pass parameters as pointers

--- include/ld2410_webserver.h
+++ include/ld2410_webserver.h
@@ -6,9 +6,13 @@
 
 namespace ld2410_rest {
 
+template <typename TReader, typename TWriter>
 class Ld2410Webserver {
 private:
     WifiWebServer m_server;
+    TReader *m_reader;
+    TWriter *m_writer;
+    Ld2410State *m_state;
 
 public:
     Ld2410Webserver(): m_server() {
@@ -17,13 +21,13 @@ public:
 
     
 
-    template <typename T, typename TReader, typename TWriter>
-    constexpr rest_service_t command_to_route(std::string route, TReader *reader, TWriter *writer) {
+    template <typename T>
+    constexpr rest_service_t command_to_route(std::string route) {
     return { MethodPut, route, [=](AsyncWebServerRequest* req, JsonDocument* res){
         auto o = res->as<JsonObject>();
         auto command = from_json<T>(o);
         o.clear();
-        auto ack = ld2410::write_and_read_ack(*writer, *reader, command, 500);
+        auto ack = ld2410::write_and_read_ack(*m_writer, *m_reader, command, 500);
         if (!ack) {
             o["error"] = "no ack packet";
         } else {
@@ -36,29 +40,33 @@ public:
 
     template <typename TReader, typename TWriter>
     void begin(AsyncWebServer* async_web_server, Ld2410State *state, TReader *reader, TWriter *writer) {
+        this->m_reader = reader;
+        this->m_writer = writer;
+        this->m_state = state;
+
         m_server.begin(async_web_server, {
             { MethodOptions, "/", [](AsyncWebServerRequest* req, JsonDocument* res){
                 res->to<JsonObject>();
 
                 return RestServiceResult(200);
             }},
-            { MethodGet, "/state", [=](AsyncWebServerRequest* req, JsonDocument* res){
+            { MethodGet, "/state", [](AsyncWebServerRequest* req, JsonDocument* res){
                 auto o = res->to<JsonObject>();
-                to_json(o, state->get_detection());
+                to_json(o, m_state->get_detection());
 
                 return RestServiceResult(200);
             }},
-            command_to_route<ld2410::EnableConfigurationCommand>("/EnableConfigurationCommand", reader, writer),
-            command_to_route<ld2410::EndConfigurationCommand>("/EndConfigurationCommand", reader, writer),
-            command_to_route<ld2410::MaximumDistanceGateandUnmannedDurationParameterConfigurationCommand>("/MaximumDistanceGateandUnmannedDurationParameterConfigurationCommand", reader, writer),
-            command_to_route<ld2410::ReadParameterCommand>("/ReadParameterCommand", reader, writer),
-            command_to_route<ld2410::EnableEngineeringModeCommand>("/EnableEngineeringModeCommand", reader, writer),
-            command_to_route<ld2410::CloseEngineeringModeCommand>("/CloseEngineeringModeCommand", reader, writer),
-            command_to_route<ld2410::RangeSensitivityConfigurationCommand>("/RangeSensitivityConfigurationCommand", reader, writer),
-            command_to_route<ld2410::ReadFirmwareVersionCommand>("/ReadFirmwareVersionCommand", reader, writer),
-            command_to_route<ld2410::SetSerialPortBaudRate>("/SetSerialPortBaudRate", reader, writer),
-            command_to_route<ld2410::FactoryReset>("/FactoryReset", reader, writer),
-            command_to_route<ld2410::RestartModule>("/RestartModule", reader, writer),
+            command_to_route<ld2410::EnableConfigurationCommand>("/EnableConfigurationCommand"),
+            command_to_route<ld2410::EndConfigurationCommand>("/EndConfigurationCommand"),
+            command_to_route<ld2410::MaximumDistanceGateandUnmannedDurationParameterConfigurationCommand>("/MaximumDistanceGateandUnmannedDurationParameterConfigurationCommand"),
+            command_to_route<ld2410::ReadParameterCommand>("/ReadParameterCommand"),
+            command_to_route<ld2410::EnableEngineeringModeCommand>("/EnableEngineeringModeCommand"),
+            command_to_route<ld2410::CloseEngineeringModeCommand>("/CloseEngineeringModeCommand"),
+            command_to_route<ld2410::RangeSensitivityConfigurationCommand>("/RangeSensitivityConfigurationCommand"),
+            command_to_route<ld2410::ReadFirmwareVersionCommand>("/ReadFirmwareVersionCommand"),
+            command_to_route<ld2410::SetSerialPortBaudRate>("/SetSerialPortBaudRate"),
+            command_to_route<ld2410::FactoryReset>("/FactoryReset"),
+            command_to_route<ld2410::RestartModule>("/RestartModule"),
         });
     }
 };
